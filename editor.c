b71c69e09b42c826b30be91b1fdd9869aeeed81b
AkdenizKutayOcal <akutay.ocal@tedu.edu.tr>
2020-04-15 22:59:31 +0300

    Sequential Mode v1

--- editor.c
+++ editor.c
@@ -2,11 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #define WORD_LEN_MAX 100                                                       
 #define LINE_LEN_MAX 512
+#define NUMBER_OF_COMMAND 10
 
-void search();  //search <keyword> [-c] <inFile> 
+/*s truct thread_args
+ {
+    char *filename;
+    int count;
+}; */
+
+struct thread_args
+{
+  /* data */
+  char* filename;
+  int isCount;
+  char* keyword;
+  char* targetKeyword;
+  char* sourceKeyword;
+  int isAorB;
+  char* insertedKeyword;
+  int charCount;
+  int lineCount;
+  int startLine;
+  int endLine;
+
+};
+
+
+void *search();  //search <keyword> [-c] <inFile> 
 //The command searches the input file for the occurrence of 
 //keyword and prints each line that contains the keyword. The The third argument 
 //"-c" is optional. If included, the command also prints the number of  times 
@@ -20,7 +46,7 @@ void replace(); //replace <targetKeyword> <sourceKeyword> [-c] <inFile>
 //the targetKeyword. -a (after) or -b (before) arguments decide whether the 
 //insertedKeyword is placed before or after the targetKeyword. 
 
-void lineCount(); //lineCount <inFile>
+void *lineCount(); //lineCount <inFile>
 // Counts the number of lines in the input File and prints it
 
 void split();  //split <CharacterCount> <inFile>
@@ -29,13 +55,13 @@ void split();  //split <CharacterCount> <inFile>
 // each line cannot have more than "CharacterCount" characters. If a line 
 //within the input fi	le contains more characters, it is divided into multiple lines.
 
-void head();  //head <lineCount> <inFile>
+void *head();  //head <lineCount> <inFile>
 // Prints the first "lineCount" lines in the input File
 
-void tail();  //tail <lineCount> <inFile>
+void *tail();  //tail <lineCount> <inFile>
 // Prints the last "lineCount" lines in the input File
 
-void mid();   // mid <startLine> <endLine> <inFile>
+void *mid();   // mid <startLine> <endLine> <inFile>
 // Prints the lines between "startline" and "endline" in the input File
 
 // Helper functions:
@@ -43,59 +69,173 @@ void mid();   // mid <startLine> <endLine> <inFile>
 void copyFile();  // copyFile <inFile> <outFile>
 // Copies entire file to another one.
 
+void midAlter();
 
 int main(int argc, char* argv[]){
 
-  char input[50];
-  char command[50];
-  char *inputptr;
+  char input[LINE_LEN_MAX];
   char filename[50] = "file.txt"; 
-  int i;
-
-  inputptr = input;
 
   printf("prompt:>");
-  gets(inputptr);
+  gets(input);
 
-  while(strcmp(inputptr, "quit")!= 0) {
+  while(strcmp(input, "quit")!= 0) {
 
-    /* printf("LineCount:\n");
-    lineCount(filename);*/
+    int i = 0;
+    int numberOfArgs = 0;
+    char commandArg[NUMBER_OF_COMMAND][WORD_LEN_MAX]; //holds all input arguments as an array seperated by spaces
 
-    /* printf("Search:\n");
-    search("software",1, filename); */
-
-    /* printf("Replace:\n");
-    replace("Lorem","software and others",1,filename);  */
-
-    /* printf("Split:\n");
-    split(90,filename); */
-
-    printf("Insert -a:\n");
-    insert("AKdeniz",1,1,"Lorem",filename);
-
-/* 
-    printf("Head:\n");
-    head(0,filename);
-
-    printf("Mid\n");
-    mid(3,5,filename);
-
-    printf("Tail:\n");
-    tail(11,filename); */
-
+    char *token = strtok(input," ");
     
+    while( token != NULL ) {    //seperating input to string array seperated by spaces
+      
+      strcpy(commandArg[i++],token);
+      numberOfArgs ++;
 
+      token = strtok(NULL," ");
+    }
+
+    int numberOfColon = 0,numberOfSemiColon = 0;
+    int colon[15], semicolon[15];
+    int numberOfThreads = 0;
+
+    for(int j=0; j<numberOfArgs;j++){     //counting the colon and semi colon which is number of threads and storing indexes
+      if(strcmp(commandArg[j],":")==0){
+        
+        colon[numberOfColon++] = j;
+      }
+      else if(strcmp(commandArg[j],":")==0){
+
+        semicolon[numberOfSemiColon++];
+      }
+    }
+
+    numberOfThreads = numberOfColon+numberOfSemiColon+1;
+
+    pthread_t tids[numberOfThreads];
+
+    if(numberOfColon>0 && numberOfSemiColon==0){  //Sequential Mode !!!!!!!!!!!!!
+      printf("Running in normal mode\n");
+
+      char filename [WORD_LEN_MAX];
+      strcpy(filename,commandArg[numberOfArgs-1]);
+
+      int start = 0, end = colon[0];
+      for(int i=0;i<numberOfThreads;i++){
+       
+        char args[10][WORD_LEN_MAX];
+        
+        if(i>=numberOfColon){
+          end = numberOfArgs;
+        }
+        if(i<numberOfColon){
+          end = colon[i];
+        }
+
+        int index = 0;
+
+        for(int k = start; k<end;k++){
+          strcpy(args[index++],commandArg[k]);
+        }
+        
+        if(strcmp(args[0],"lineCount")==0){
+          
+          struct thread_args lineCountArgs;
+          lineCountArgs.filename = args[1]; 
+          
+          pthread_create(&tids[i],NULL, lineCount,&lineCountArgs);
+          pthread_join(tids[i],NULL);
+        }
+
+        else if(strcmp(args[0],"tail")==0){
+          
+          struct thread_args tailArgs;
+          tailArgs.charCount = atoi(args[1]);
+          tailArgs.filename = args[2];
+
+          pthread_create(&tids[i],NULL, tail,&tailArgs);
+          pthread_join(tids[i],NULL);
+        }
+
+        else if(strcmp(args[0],"head")==0){
+         
+          struct thread_args headArgs;
+          headArgs.charCount = atoi(args[1]);
+          headArgs.filename = args[2];
+
+          pthread_create(&tids[i],NULL,head,&headArgs);
+          pthread_join(tids[i],NULL);
+        }
+
+        else if(strcmp(args[0],"mid")==0){
+         //Problem with mid file does not open
+
+          struct thread_args midArgs;
+          
+          midArgs.filename = args[3];
+
+          pthread_create(&tids[i],NULL,mid,&midArgs);
+          pthread_join(tids[i],NULL);
+        }
+
+        else if(strcmp(args[0],"search")==0){
+         
+          struct thread_args searchArgs;
+
+          if(index==3){
+            searchArgs.keyword = args[1];
+            searchArgs.filename = args[2];
+            searchArgs.isCount = 0;
+          }
+          else if(index == 4){
+            searchArgs.keyword = args[1];
+            searchArgs.filename = args[3];
+            searchArgs.isCount = 1;
+          }
+        
+          pthread_create(&tids[i],NULL,search,&searchArgs);
+          pthread_join(tids[i],NULL);
+        }
+
+/*         else if(strcmp(args[0],"replace")==0){
+         
+          struct thread_args replaceArgs;
+
+          if(index==3){
+            searchArgs.keyword = args[1];
+            searchArgs.filename = args[2];
+            searchArgs.isCount = 0;
+          }
+          else if(index == 4){
+            searchArgs.keyword = args[1];
+            searchArgs.filename = args[3];
+            searchArgs.isCount = 1;
+          }
+        
+          pthread_create(&tids[i],NULL,search,&searchArgs);
+          pthread_join(tids[i],NULL);
+        } */
+        start = end+1;
+        printf("\n----------------------------------------\n");
+      }
+    }
+       
+    
     printf("\nprompt:>");
-    gets(inputptr);
+    gets(input);
   }
 
   return 0;
 
 }
 
-void search(char keyword[], int isCount, char filename[]){
+void *search(void *ptr){
   // isCOunt == 0 when -c is not entered 1 otherwise
+
+  struct thread_args *args = (struct thread_args *)ptr;
+  char *filename = args->filename;
+  char *keyword = args->keyword;
+  int isCount = args->isCount;
 
   FILE*fp;
 
@@ -278,9 +418,12 @@ void insert(char insertedKeyword[], int isCount, int isAorB, char targetKeyword[
   fclose(fp_temp);
   
 }  
-void lineCount(char filename[]){
+void *lineCount(void *ptr){
   // count +1 maybe
-
+  
+  struct thread_args *args = (struct thread_args *)ptr;
+  char *filename = args->filename;
+  
   int count = 1;
   FILE*fp;
 
@@ -398,7 +541,11 @@ void split(int charCount, char filename[]){
   }
 }
 
-void head(int lineCount, char filename[]){
+void *head(void *ptr){
+
+  struct thread_args *args = (struct thread_args *)ptr;
+  char *filename = args->filename;
+  int lineCount = args->charCount;
 
   FILE*fp;
 
@@ -425,8 +572,12 @@ void head(int lineCount, char filename[]){
     }
   }
 }
-void tail(int lineCount, char filename[]){
+void *tail( void *ptr){
   
+  struct thread_args *args = (struct thread_args *)ptr;
+  char *filename = args->filename;
+  int lineCount = args->charCount;
+
   FILE*fp;
 
   fp = fopen(filename,"r");
@@ -450,12 +601,18 @@ void tail(int lineCount, char filename[]){
     }
     else{
       int startLine = totalLines-lineCount;
-      mid(startLine+1,totalLines,filename);
+      midAlter(startLine+1,totalLines,filename);
     }
   }
 
 }
-void mid(int startLine, int endLine, char filename[]){
+
+void *mid(void *ptr){
+
+  struct thread_args *args = (struct thread_args *)ptr;
+  char *filename = args->filename;
+  int startLine = args->startLine;
+  int endLine = args->endLine;
 
   FILE*fp;
 
@@ -496,4 +653,33 @@ void copyFile(FILE *in, FILE *out){
       fwrite(line, 1, strlen(line), in);
     }
         
+}
+
+void midAlter(int startLine, int endLine, char filename[]){
+
+  FILE*fp;
+
+  fp = fopen(filename,"r");
+  char temp;
+  
+  if(fp == NULL){
+    printf("Failed to open the file %s",filename);
+  }
+  else if(startLine==0){
+    printf("startLine has to be more than 0.\n");
+  }
+  else if(startLine>endLine){
+    printf("Please enter valid range.\n");
+  }
+
+  else{
+    int count = 0;
+    char line[LINE_LEN_MAX];
+    while(fgets(line, sizeof(line), fp) != NULL){
+      count++;
+      if(count>=startLine && count<=endLine){
+        printf("%s",line);
+      }
+    }
+  }
 }
